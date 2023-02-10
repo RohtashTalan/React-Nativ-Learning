9e2a65a2958ddf9ca5f4d9d0e31c94b136b79f60
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-02-11 01:44:26 +0530

    currency app done

--- CurrencyApp05/App.js
+++ CurrencyApp05/App.js
@@ -1,93 +1,127 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
 import {
-  Text, 
+  Text,
   View,
   ScrollView,
   SafeAreaView,
   StyleSheet,
   TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
-
-
-
-} from "react-native";
-
-
+import Snackbar from 'react-native-snackbar';
 
 const currencyPerRupee = {
-    DOLLAR: 0.014,
-    EURO: 0.12,
-    POUND: 0.93,
-    AUSDOLLAR: 0.2,
-    CANDOLLAR: 0.019,
-    YEN: 1.54,
-    DINAR: 0.0043,
-    BITCOIN: 0.00004
-}
-
+  DOLLAR: 0.014,
+  EURO: 0.12,
+  POUND: 0.93,
+  AUSDOLLAR: 0.2,
+  CANDOLLAR: 0.019,
+  YEN: 1.54,
+  DINAR: 0.0043,
+  BITCOIN: 0.00004,
+};
 
 const App = () => {
   const [inputValue, setInputValue] = useState(0);
   const [resultValue, setResultValue] = useState(0);
 
+  const buttonPressed = currency => {
+    if (!inputValue) {
+      return Snackbar.show({
+        text: 'Please enter a value',
+        backgroundColor: '#EA7773',
+        textColor: '#FFFFFF',
+      });
+    }
 
+    let result = parseFloat(inputValue) * currencyPerRupee[currency];
+    setResultValue(result.toFixed(2));
+  };
   return (
     <>
-      <ScrollView backgroundColor="#1b262c">
+      <ScrollView
+        backgroundColor="#1b262c"
+        keyboardShouldPersistTaps="handled"
+        contentInsetAdjustmentBehavior="automatic">
         <SafeAreaView style={styles.container}>
-           <View style={styles.resultContainer}>
-            <Text style={styles.resultValue}>12.23</Text>
-           </View>
-           <View style={styles.inputContainer}>
-            <TextInput style={styles.input}
-            keyboardType="numeric"
-            placeholder="Enter Value"
-            placeholderTextColor="#c1c1c1"
+          <View style={styles.resultContainer}>
+            <Text style={styles.resultValue}>{resultValue}</Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              keyboardType="numeric"
+              placeholder="Enter Value"
+              placeholderTextColor="#c1c1c1"
+              value={inputValue}
+              onChangeText={t => setInputValue(t)}></TextInput>
+          </View>
 
-            >
-
-            </TextInput>
-           </View>
+          <View style={styles.convertButtonContainer}>
+            {Object.keys(currencyPerRupee).map((currency, i) => (
+              <TouchableOpacity
+                key={i}
+                onPress={() => buttonPressed(currency)}
+                style={styles.converterButton}>
+                <Text>{currency}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
         </SafeAreaView>
       </ScrollView>
     </>
   );
-}
-
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#1b262c"
+    backgroundColor: '#1b262c',
   },
   resultContainer: {
     height: 70,
     marginTop: 80,
-    justifyContent: "center",
-    borderColor: "#bbe1fa",
+    justifyContent: 'center',
+    borderColor: '#bbe1fa',
     borderWidth: 2,
-    alignItems:"center"
+    alignItems: 'center',
   },
   resultValue: {
     fontSize: 30,
-    color: "#fff",
-    fontWeight: "bold"
+    color: '#fff',
+    fontWeight: 'bold',
   },
 
   inputContainer: {
     height: 70,
     marginTop: 10,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     borderWidth: 2,
-    borderColor: "#bbe1fa",
-
+    borderColor: '#bbe1fa',
   },
-  input:{
-    fontSize:30,
-    textAlign: "center",
-    color: "#fff"
-  }
-})
+  input: {
+    fontSize: 30,
+    textAlign: 'center',
+    color: '#fff',
+  },
+  convertButtonContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 30,
+    gap: 10,
+  },
+  converterButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 100,
+    borderWidth: 2,
+    borderColor: '#bbe1fa',
+    backgroundColor: '#0f4c75',
+    width: '31%',
+    borderRadius: 5,
+  },
+});
 
 export default App;

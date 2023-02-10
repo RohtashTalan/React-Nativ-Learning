ffdc2c01cb4747f2b4ee4cd06402a4fc369474e5
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-02-10 07:30:13 +0530

    rename app file

---
+++ SpanishNumber04/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+import {
+Text,
+View,
+
+} from 'react-native'
+
+
+
+const App = () => {
+
+  return(
+    <>
+    <Text>
+hello
+    </Text>
+    </>
+  )
+}
+
+
+export default App;

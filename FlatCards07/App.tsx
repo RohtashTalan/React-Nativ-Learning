a569ec91057643d05970f1cde64d939862c6610e
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-02-16 07:59:21 +0530

    action card added

--- FlatCards07/App.tsx
+++ FlatCards07/App.tsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import FlatCards from './components/FlatCards'
 import ElevatedCards from './components/ElevatedCards'
 import FancyCard from './components/FancyCard'
+import ActionCard from './components/ActionCard'
 
 export class App extends Component {
   render() {
@@ -12,6 +13,7 @@ export class App extends Component {
           <FlatCards />
           <ElevatedCards />
           <FancyCard />
+          <ActionCard />
         </ScrollView>
       </SafeAreaView>
     )

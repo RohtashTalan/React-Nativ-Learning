1f027c14db3e4df991c1c3070f02adebb3ec9e16
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-02-17 07:27:17 +0530

    netlfix config and screen file

--- NetflixStore09/screens/Add.tsx
+++ NetflixStore09/screens/Add.tsx
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const App = () => {
+const Add = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Text>Add</Text>
     </View>
   )
 }
 
-export default App
+export default Add
 
 const styles = StyleSheet.create({})

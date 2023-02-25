ff6eb604bccdba001926840a0b982f97579ea473
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-02-25 07:11:30 +0530

    done

--- reduxApp11/android/app/src/main/java/com/reduxapp11/MainActivity.java
+++ reduxApp11/android/app/src/main/java/com/reduxapp11/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "reduxApp11";
+    return "ReduxApp11";
   }
 
   /**
--- reduxApp11/ios/reduxApp11Tests/reduxApp11Tests.m
+++ reduxApp11/ios/reduxApp11Tests/reduxApp11Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reduxApp11Tests : XCTestCase
+@interface ReduxApp11Tests : XCTestCase
 
 @end
 
-@implementation reduxApp11Tests
+@implementation ReduxApp11Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {

db08b5b04fad1cc9c2cdc44d3878d0cd5015f688
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-03-02 07:17:30 +0530

    auth complete

---
+++ SocialApp12/src/store/slices/auth.js
@@ -0,0 +1,94 @@
+import auth from '@react-native-firebase/auth';
+import Snackbar from 'react-native-snackbar';
+import database from '@react-native-firebase/database'
+import { firebase_db } from '../../database';
+
+
+
+export const signUp = (data) => async(dispatch) => {
+    const {name, instaUserName, bio, email, password, country, image} = data
+    
+    auth().createUserWithEmailAndPassword(email, password)
+    .then((data) => {
+        console.log(data);
+
+        database(firebase_db)
+        .ref('/users/'+data.user.uid)
+        .set({
+            name,
+            instaUserName,
+            country,
+            image,
+            bio,
+            uid: data.user.uid
+        })
+        .then(()=> console.log("Data set Success"))
+
+        Snackbar.show({
+            text:"Account created",
+            textColor:"white",
+            backgroundColor:"#1b262c"
+        })
+
+    })
+    .catch((error)=>{
+        console.error(error);
+        Snackbar.show({
+            text: "Signup failed",
+            textColor: "White",
+            backgroundColor: "red"
+        })
+    })
+}
+
+
+export const signIN = (data) => async(dispatch) => {
+   
+    const {email, password} = data
+
+    auth().signInWithEmailAndPassword(email, password)
+    .then(()=>{
+        console.log("Sign in success");
+        Snackbar.show({
+            text:"Account signin",
+            textColor:"white",
+            backgroundColor:"#1b2b2c"
+        })
+    })
+    .catch((error)=>{
+        console.error(error);
+        Snackbar.show({
+            text: "Signin failed",
+            textColor:"white",
+            backgroundColor:"red"
+        })
+    })
+}
+
+export const signOut = () => async(dispatch) => {
+
+    auth()
+    .signOut()
+    .then(()=>{
+        console.log("signout success");
+        Snackbar.show({
+            text:"SignOut Successful",
+            textColor:"white",
+            backgroundColor:"#1b2b2c"
+        })
+    })
+    .catch((error)=>{
+        console.error(error);
+        Snackbar.show({
+            text: "SignOut failed",
+            textColor:"white",
+            backgroundColor:"red"
+        })
+    })
+
+
+
+}
+
+
+

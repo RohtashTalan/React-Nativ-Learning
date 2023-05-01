c1dad3dd03c021152fcca318311d06f8f557276a
Rohtash Talan <rohtashtalancompany@gmail.com>
2023-05-01 07:19:31 +0530

    navigation stack

---
+++ navigationStack/src/App.tsx
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+//Navigation
+import { NavigationContainer } from '@react-navigation/native';
+
+function App(): JSX.Element {
+
+
+  return (
+    <SafeAreaView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
